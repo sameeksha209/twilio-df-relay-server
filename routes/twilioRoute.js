fa67cd8200d208974db55d78ffeaa12d8ea084e6
sameeksha.chourasiya <sameeksha.chourasiya@accenture.com>
2026-01-01 13:09:22 +0530

    sending streaming ur l

--- routes/twilioRoute.js
+++ routes/twilioRoute.js
@@ -1,32 +1,43 @@
 const express = require("express");
 const router = express.Router();
 const jwt = require("jsonwebtoken");
+const twilio = require('twilio')
 const JWT_SECRET = process.env.STREAM_JWT_SECRET; // store in Cloud Run env
 const JWT_EXPIRY = "20s"; // short lived
 const StreamingUrl = 'wss://istha-twilio-streaming-server-100251281488.us-central1.run.app/streaming'
+const twiml = new twilio.twiml.VoiceResponse();
+
 router.get("/", (req, res) => res.send("send successfully"));
 
 router.get("/call-start", (req, res) => res.send("OK"));
 
 router.post("/call-start", async (req, res) => {
-  console.log("Twilio call-start webhook hit",req,res);
+  console.log("Twilio call-start webhook hit", req.body, req.body.callsid);
   		
-  const jwtPayload = { callSid: 'acmsdifvdifh2234efsdcvsrd' };
+  const jwtPayload = { callSid: req.body.callsid };
   const token = generateStreamToken(jwtPayload);
-    const twiml = `
-<Response>
-  <Start>
-    <Stream url="${StreamingUrl}">
-      <Parameter name="token" value="${token}" />
-    </Stream>
-  </Start>
-  <Say>Streaming started!</Say>
-</Response>
-`;
+   const connect = twiml.connect();
+
+  // STREAM URL (no quotes)
+  connect.stream({
+    url: StreamingUrl,
+    token: token
+  });
+
+//     const twiml = `
+// <Response>
+//   <Start>
+//     <Stream url="${StreamingUrl}">
+//       <Parameter name="token" value="${token}" />
+//     </Stream>
+//   </Start>
+//   <Say>Streaming started!</Say>
+// </Response>
+// `;
   console.log('send twiml');
  
 res.set("Content-Type", "text/xml");
-res.send(twiml);
+res.send('done');
 
 });
 
